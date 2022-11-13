6469404b18780b9c7cbefe3831b3a71cf9640198
okabe <nagi0110@yahoo.co.jp>
2022-11-13 11:24:22 +0900

    プロジェクトの追加

---
+++ BlazorBase.Domain/Framework/IUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorBase.Domain.Framework
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        void Begin();
+        void Commit();
+        void Rollback();
+
+        void Save(Action action);
+    }
+}
---
+++ BlazorBase.Infrastructure/UnitOfWork.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Storage;
+using System;
+using BlazorBase.Domain.Framework;
+using BlazorBase.Infrastructure.Contexts;
+
+namespace BlazorBase.Infrastructure
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        protected readonly BlazorBaseContext context;
+        protected IDbContextTransaction transaction;
+
+        public UnitOfWork(BlazorBaseContext context)
+        {
+            this.context = context;
+        }
+
+        public void Begin()
+        {
+            transaction = context.Database.BeginTransaction();
+        }
+
+        public void Commit()
+        {
+            transaction.Commit();
+            Dispose();
+        }
+
+        public void Rollback()
+        {
+            transaction.Rollback();
+            Dispose();
+        }
+
+        public void Save(Action action)
+        {
+            try
+            {
+                Begin();
+                action();
+                Commit();
+            }
+            catch
+            {
+                Rollback();
+                throw;
+            }
+        }
+
+        public void Dispose()
+        {
+            if (transaction == null) return;
+
+            transaction.Dispose();
+        }
+    }
+}
