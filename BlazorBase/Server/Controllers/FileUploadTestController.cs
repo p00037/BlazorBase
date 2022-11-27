261c0bcc2f18546d7d5148d115ced0ec6f6aa55e
okabe <nagi0110@yahoo.co.jp>
2022-11-27 21:08:48 +0900

    [update] #11 ファイルのアップロードの作成

---
+++ BlazorBase/Server/Controllers/FileUploadTestController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BlazorBase.Server.Controllers
+{
+    //[Route("api/[controller]")]
+    [ApiController]
+    public class FileUploadTestController : ControllerBase
+    {
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public FileUploadTestController(IWebHostEnvironment webHostEnvironment)
+        {
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+        [HttpPost("upload/multiple")]
+        public async Task<IActionResult> OnPostUploadAsync([FromForm(Name = "files")] ICollection<IFormFile> files)
+        {
+            long size = files.Sum(f => f.Length);
+
+            foreach (var formFile in files)
+            {
+                if (formFile.Length > 0)
+                {
+                    var fileName = formFile.FileName;
+                    //var filePath = Path.GetTempFileName();
+                    var filePath = $"{_webHostEnvironment.WebRootPath}\\upload\\{fileName}";
+
+                    using (var stream = System.IO.File.Create(filePath))
+                    {
+                        await formFile.CopyToAsync(stream);
+                    }
+                }
+            }
+
+            // Process uploaded files
+            // Don't rely on or trust the FileName property without validation.
+
+            return Ok(new { count = files.Count, size });
+        }
+    }
+}
