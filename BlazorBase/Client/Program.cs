157b5fbc23c11281e48966021579cb8c45d7155a
okabe <nagi0110@yahoo.co.jp>
2022-12-28 21:51:48 +0900

    [add] #29 ログイン認証の登録をコントローラーで行うように変更
    必要ないファイルを削除

--- BlazorBase/Client/Program.cs
+++ BlazorBase/Client/Program.cs
@@ -22,7 +22,6 @@ builder.Services.AddScoped<IAPIService, APIService>();
 builder.Services.AddScoped<MstOfficeClient>();
 builder.Services.AddScoped<DownloadTestClient>();
 builder.Services.AddScoped<PdfShowClient>();
-builder.Services.AddScoped<AccountClient>();
 builder.Services.AddScoped<MstLoginUserClient>();
 
 builder.Services.AddScoped<NotificationService>();
